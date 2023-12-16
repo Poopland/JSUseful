d8e4ea8f761934343fd949cdd6b000afc17b6240
Megumint <66193126+Poopland@users.noreply.github.com>
2023-12-17 00:16:34 +0700

    Update Roblox : Show Total Robux in Purchase Transaction.js

--- Roblox : Show Total Robux in Purchase Transaction.js
+++ Roblox : Show Total Robux in Purchase Transaction.js
@@ -1,11 +1,9 @@
 setInterval(() => {
-    if(document.getElementsByClassName("rbx-selection-label")[0].textContent == "Purchases"){
-        let purchased = document.getElementsByClassName("amount icon-robux-container")
-        let total = 0
-        for (let i=0;i<purchased.length;i++){
-            total += Number(purchased[i].childNodes[2].textContent.replace(",",""))
-        }
-        
-        document.getElementsByClassName("amount")[0].textContent = "Total : " + total
+    let purchased = document.getElementsByClassName("amount icon-robux-container")
+    let total = 0
+    for (let i=0;i<purchased.length;i++){
+        total += Number(purchased[i].childNodes[2].childNodes[0].textContent.replaceAll(",", ''))
     }
+
+    document.getElementsByClassName("amount")[0].textContent = "Total : " + total.toLocaleString()
 }, 100)
